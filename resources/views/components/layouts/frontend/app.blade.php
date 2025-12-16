b7ce82b58f98be9da78116cae8009a6cdadf96c4
Habibur Rahaman <shamolrahaman@gmail.com>
2025-12-16 17:04:31 +0600

    Refine Prism highlighting for PHP, HTML, and JS

--- resources/views/components/layouts/frontend/app.blade.php
+++ resources/views/components/layouts/frontend/app.blade.php
@@ -9,7 +9,6 @@
         <x-seo.meta :seo="$seoData" />
         {{-- Google Font --}}
         <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-tomorrow.min.css" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
         @livewireStyles
@@ -51,6 +50,8 @@
         @livewireScripts
         <script src="{{ asset('assets/js/script.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-markup.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-markup-templating.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-php.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-javascript.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-css.min.js"></script>
