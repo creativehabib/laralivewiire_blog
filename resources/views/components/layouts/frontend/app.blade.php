b4134829113422d4416a30128b33cbf9217c2406
creativehabib <shamolrahaman@gmail.com>
2025-12-24 01:58:40 +0600

    update

--- resources/views/components/layouts/frontend/app.blade.php
+++ resources/views/components/layouts/frontend/app.blade.php
@@ -12,7 +12,9 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
 
     <style>
         .animate-marquee { display: inline-block; animation: marquee 18s linear infinite; }
