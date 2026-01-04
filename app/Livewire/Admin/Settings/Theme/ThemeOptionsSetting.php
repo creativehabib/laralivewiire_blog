78c44820f99c8ad00f4f479b3612fc5b2218726f
creativehabib <shamolrahaman@gmail.com>
2026-01-05 01:22:32 +0600

    update

--- app/Livewire/Admin/Settings/Theme/ThemeOptionsSetting.php
+++ app/Livewire/Admin/Settings/Theme/ThemeOptionsSetting.php
@@ -8,6 +8,8 @@ class ThemeOptionsSetting extends Component
 {
     public function render()
     {
-        return view('livewire.admin.settings.theme.theme-options-setting');
+        return view('livewire.admin.settings.theme.theme-options-setting')->layout('components.layouts.app', [
+            'title' => 'Theme Options',
+        ]);
     }
 }
