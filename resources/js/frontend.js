81acd4b2f64a6132fc8723b8e3b400cae7ced9a4
Habibur Rahaman <shamolrahaman@gmail.com>
2026-04-01 00:27:57 +0600

    Use toggle switch for scroll-to-top enable control

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -1,5 +1,18 @@
 import { setupCodeCopy } from './modules/code-copy';
 import { registerServiceWorker } from './modules/pwa';
+import { setupScrollToTop } from './modules/scroll-to-top';
 
 setupCodeCopy();
 registerServiceWorker();
+
+const initFrontendUi = () => {
+    setupScrollToTop();
+};
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initFrontendUi, { once: true });
+} else {
+    initFrontendUi();
+}
+
+document.addEventListener('livewire:navigated', initFrontendUi);
